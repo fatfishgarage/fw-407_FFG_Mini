648770309b55b0d9dc1825818922de4afb3ac0c5
GitHub git update Action <action@github.com>
2026-01-04 01:20:39 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_FFG_Mini.h
+++ generated/controllers/generated/rusefi_generated_FFG_Mini.h
@@ -1546,7 +1546,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 4247212794
+#define SIGNATURE_HASH 333823165
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2137,7 +2137,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2026.01.03.FFG_Mini.4247212794"
+#define TS_SIGNATURE "rusEFI main.2026.01.04.FFG_Mini.333823165"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_FFG_Mini.h
+++ generated/controllers/generated/signature_FFG_Mini.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4247212794
-#define TS_SIGNATURE "rusEFI main.2026.01.03.FFG_Mini.4247212794"
+#define SIGNATURE_HASH 333823165
+#define TS_SIGNATURE "rusEFI main.2026.01.04.FFG_Mini.333823165"
