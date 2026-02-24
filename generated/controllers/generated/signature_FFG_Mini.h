53518d0ae115a729e38a7205821ee62af04c9398
GitHub git update Action <action@github.com>
2026-02-24 01:28:34 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_FFG_Mini.h
+++ generated/controllers/generated/signature_FFG_Mini.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 29675757
-#define TS_SIGNATURE "rusEFI main.2026.02.23.FFG_Mini.29675757"
+#define SIGNATURE_HASH 84444525
+#define TS_SIGNATURE "rusEFI main.2026.02.24.FFG_Mini.84444525"
