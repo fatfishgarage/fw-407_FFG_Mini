10375852d1206681b5521805fe8cdb5654e40998
GitHub git update Action <action@github.com>
2026-05-10 02:16:18 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_FFG_Mini.h
+++ generated/controllers/generated/rusefi_generated_FFG_Mini.h
@@ -2178,7 +2178,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.05.09.FFG_Mini.3600232985"
+#define TS_SIGNATURE "rusEFI main.2026.05.10.FFG_Mini.3600232985"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
